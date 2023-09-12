8fba15f4bcb95ed35e85c08bd9593520530f8a98
ircamitoc <iraincamitoc@gmail.com>
2023-09-12 21:59:30 +0800

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import "./App.css";
 import gptLogo from "./assets/chatgpt.svg";
+import addBtn from "./assets/add-30.png";
+import msgIcon from "./assets/message.svg";
+import home from "./assets/home.svg";
+import saved from "./assets/bookmark.svg";
+import rocket from "./assets/rocket.svg";
 
 function App() {
   return (
@@ -9,25 +14,38 @@ function App() {
           <div className="upperSideTop">
             <img src={gptLogo} alt="" className="logo" />
             <span className="brand">ChatGPT</span>
-            <button className="midBtn">
-              <img src="" alt="" className="addBtn" />
-              New Chat
+          </div>
+          <button className="midBtn">
+            <img src={addBtn} alt="new chat" className="addBtn" />
+            New Chat
+          </button>
+          <div className="upperSideBottom">
+            <button className="query">
+              <img src={msgIcon} alt="Query" />
+              What is Programming?
             </button>
-            <div className="upperSideBottom">
-              <button className="query">
-                <img src="" alt="" />
-                What is Programming?
-              </button>
-              <button className="query">
-                <img src="" alt="" />
-                What is Programming?
-              </button>
-            </div>
+            <button className="query">
+              <img src={msgIcon} alt="Query" />
+              How to use an API?
+            </button>
           </div>
         </div>
-        <div className="lowerSide"></div>
-        <div className="main"></div>
+        <div className="lowerSide">
+          <div className="listItems">
+            <img src={home} alt="" className="listItemsImg" />
+            Home
+          </div>
+          <div className="listItems">
+            <img src={saved} alt="" className="listItemsImg" />
+            Saved
+          </div>
+          <div className="listItems">
+            <img src={rocket} alt="" className="listItemsImg" />
+            Upgrade to Pro
+          </div>
+        </div>
       </div>
+      <div className="main"></div>
     </div>
   );
 }
